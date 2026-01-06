2c9b1b69cef6c19dd1a40bc5e9a6631ba4db49d2
chrispaganon <info@chrispaganon.com>
2026-01-06 06:31:41 +0100

    add exa and plain ls support

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -11,12 +11,22 @@ if [[ -z $TMUX ]]; then
   exit 1
 fi
 
+ls_command() {
+  if command -v exa >/dev/null 2>&1; then
+    exa -a --color=always "$1"
+  elif command -v lsd >/dev/null 2>&1; then
+    lsd -A --color=always "$1"
+  else
+    ls -A --color=always "$1"
+  fi
+}
+
 print_new_directory_content() {
   current_path=$(tmux display-message -p -t "$pane_id" '#{pane_current_path}')
 
   if [[ "$current_path" != "$last_path" ]]; then
     echo "$current_path"
-    lsd -A --color=always "$current_path"
+    ls_command "$current_path"
     echo ""
     
     last_path="$current_path"
