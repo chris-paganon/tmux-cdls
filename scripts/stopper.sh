52732c6fd071f87fc3dff4868d0823df621d0b23
chrispaganon <info@chrispaganon.com>
2026-01-06 06:39:31 +0100

    better pane above targetting

--- scripts/stopper.sh
+++ scripts/stopper.sh
@@ -3,5 +3,4 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENT_PANE_ID=$(tmux list-panes | grep '(active)' | grep -o '%[0-9]\+')
 
-# Select the pane above and kill it
-tmux select-pane -U \; kill-pane \; select-pane -t "$CURRENT_PANE_ID"
+tmux kill-pane -t {up-of}
